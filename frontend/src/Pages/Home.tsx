767f7e4c3a829898a013ae4fa15db76182e585c6
unknown <rai2889@gmail.com>
2023-09-01 17:54:56 +0530

    homepage done

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -21,15 +21,15 @@ const Home: React.FC = () => {
     </div>
       <div className="mx-auto max-w-2xl py-10 sm:py-10 lg:py-10 mt-16"> {/* Updated mt-0 to mt-16 */}
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl" style={{fontSize:"bold" ,}}>
             Level Up Your Interview Game with AI!
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p  style={{color:'gray', textAlign:"center",fontSize:"large",}}>
             Unlock your tech potential with our AI interview platform! Seamlessly practice coding challenges, master problem-solving, and receive personalized feedback. Elevate your confidence and skills to ace any tech interview. Land your dream job with ease and efficiency!
           </p>
          <div className="mt-10 flex items-center justify-center gap-x-6" style={{display:"flex",justifyContent:"space-evenly",gap:"20vh", marginTop:"20vh"}}>
   <Link to={`/interview/java`}>
-    <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700" style={{borderRadius:"lg", background:"white", boxShadow:"0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)",backgroundColor:"dark-bg-neutral-700"}}>
       <div className="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
         <img
           className="rounded-t-lg"
@@ -46,7 +46,7 @@ const Home: React.FC = () => {
 
   <Link to={`/interview/mern`}>
     <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <div className="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
+      <div className="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light" style={{borderRadius:"lg", background:"white", boxShadow:"0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)",backgroundColor:"dark-bg-neutral-700"}}>
         <img
           className="rounded-t-lg"
           src="https://miro.medium.com/v2/resize:fit:828/format:webp/0*kxPYwfJmkXZ3iCWy.png"
@@ -62,7 +62,7 @@ const Home: React.FC = () => {
 
   <Link to={`/interview/nodejs`}>
     <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <div className="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
+      <div className="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light" style={{borderRadius:"lg", background:"white", boxShadow:"0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)",backgroundColor:"dark-bg-neutral-700"}}>
         <img
           className="rounded-t-lg"
           src="https://static.ebayinc.com/static/assets/Uploads/Stories/Articles/node.js.jpg"
